2ba2c433525f4868870926def0ae112f3b4f021c
Andrey-Babich <babichandrey18@gmail.com>
2026-05-23 12:43:45 +0300

    fix render deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ymg3m^vn+oqk+qrzuqbxlhs#6(x#fe#t-)&t4hgmn^utj9d4lk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['barbershop-crm.onrender.com', '127.0.0.1']
 
 
 # Application definition
